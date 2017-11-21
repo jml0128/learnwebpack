1f5605ddf910b237a8efea6138373f7b0248f8ce
jml0128 <361029007@qq.com>
2017-11-22 01:54:04 +0800

    cs

--- src/js/index.js
+++
@@ -1,9 +0,0 @@
-import _ from 'lodash';
-import '../css/index.css';
-import {add} from './print.js';
-
-
-var app = document.getElementById('app');
-app.innerHTML = 'hi mary';
-app.onclick = function(){alert(add(1,3));};
-
--- src/js/print.js
+++
@@ -1,6 +0,0 @@
-export function printMe() {
-  console.log('Updating print.js...111')
-}
-export function add(a,b) {
-  return a+b;
-}
